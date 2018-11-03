3e71126d11c226a4e8149a64d8c86fae9fdd575e
yapancha <YoshiPanchu1995>
2018-11-02 22:49:23 -0400

    5.  Create a unit test to insert a user into the users table.

--- tests/Feature/PagesTest.php
+++ tests/Feature/PagesTest.php
@@ -13,13 +13,37 @@ class PagesTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testExample1()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+
+        echo "\n\nRegister page sent code 200";
+    }
+    public function testExample2()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+
+        echo "\n\nLogin page sent code 200";
+    }
+    public function testExample3()
     {
         $response = $this->get('/contact');
 
         $response->assertStatus(200);
 
         echo "\n\nContact page sent code 200";
+    }
+    public function testExample4()
+    {
+        $response = $this->get('/about');
+
+        $response->assertStatus(200);
+
+        echo "\n\nAbout page sent code 200";
     }
 
 
